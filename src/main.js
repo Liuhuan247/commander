ba19f8ff7390cdad8d26f5a92462d752636e21ca
henry.liu <liuhuan2472014@gmail.com>
2021-08-04 22:52:43 +0800

    feat: :sparkles: options version parse opts 等命令
    帮助文档，命令参数等

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 const Commander = require('commander');
-const { version } = require('./utils/constants');
+const { version, name } = require('./utils/constants');
 
-// const { program } = Commander;
+const { program } = Commander;
 
 // program.version(version).parse(process.argv);
 
-const program2 = new Commander.Command();
-program2.version(version).parse(process.argv);
+// const program = new Commander.Command();
+program
+  .name(name)
+  .usage('<命令> [选项]')
+  .version(version, '-V, --version', '输出h-cli版本号')
+  .helpOption('-h, --help', '显示帮助命令')
+  .option('-d, --debug', '是否开启调试模式', 'false')
+  .option('-e, --envName <envName>', '获取环境变量')
+  .parse(process.argv);
+
+program.outputHelp();
+
+console.log(program.opts());
